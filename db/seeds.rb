0e4a19b389de5d6b2c0dfc16e553d98e7573cb82
Luis Zambrano <lzambra2@yorku.ca>
2024-01-11 16:31:30 -0500

    Adding some seed student data for dev. Fixing alerts messages. #8 Missing app/views/students/_form.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,5 @@ if (Rails.env != 'test') && User.all.count.zero?
     user.save
   end
 end
+
+10.times { |i| Student.create(username: "username#{i}" , name: "name#{i}", email: "email#{i}@domain.com", created_by_id: 1, blocked: false, role: User::STUDENT, sisid: "10000000#{i}") } if Rails.env.development?
