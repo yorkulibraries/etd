931a248f4c8eb36366e698cb29784861c4e3f3c2
Luis Zambrano <lzambra2@yorku.ca>
2024-01-29 15:33:54 -0500

    Status per thesis.

--- app/helpers/student_view_helper.rb
+++ app/helpers/student_view_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module StudentViewHelper
+    def show_accordion(current_status)
+        "accordion-collapse collapse" + (@thesis.status == current_status ? "show" : "")
+    end
+    def show_badge(current_status)
+        @thesis.status == current_status ? "badge text-bg-primary" : "badge text-bg-dark"
+    end
 end
