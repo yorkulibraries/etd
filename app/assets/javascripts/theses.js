9163a4aa9856af7f9803db4308b4aba1b5f6020d
Luis Zambrano <lzambra2@yorku.ca>
2024-02-01 12:23:27 -0500

    Removing badges, aligning titles and templates. #15 #16

--- app/assets/javascripts/theses.js
+++ app/assets/javascripts/theses.js
@@ -1,43 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	/* FORM */
-	//$('#select_subjects_1, #select_subjects_2, #select_subjects_3').chosen({ max_selected_options: 1, allow_single_deselect: true });
 	$('.select_subjects_fields').chosen({ max_selected_options: 1, allow_single_deselect: true });
-	
-  $('#thesis_exam_date').datepicker({ dateFormat: 'yy-mm-dd' });
-	
+
+	$('#thesis_exam_date').datepicker({ dateFormat: 'yy-mm-dd' });
+
 	/** STATUS NOTIFICATIONS **/
-  const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-  const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl, {
-	  content: $("#status_change").html(),
-	  html: true,
-	  sanitize: false,
-  }));
-	
+	const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+	const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl, {
+		content: $("#status_change").html(),
+		html: true,
+		sanitize: false,
+	}));
+
 	/* ReadOnly Field View */
-	$(".readonly-field a").click(function() {
+	$(".readonly-field a").click(function () {
 		$(this).parent().hide();
 		var show_field = $(this).data("id");
 		$("#" + show_field).show();
 		return false;
 	});
-	
-	
-	$(".simple_form").on("click", ".remove_fields", function(event) {
+
+
+	$(".simple_form").on("click", ".remove_fields", function (event) {
 		$(this).prev("input[type=hidden]").val("1");
 		$(this).closest("fieldset").hide();
 		event.preventDefault();
 	});
 
-	$(".simple_form").on("click", ".add_fields", function(event) {
+	$(".simple_form").on("click", ".add_fields", function (event) {
 		var time = new Date().getTime();
 		var regexp = new RegExp($(this).data("id"), "g");
-		$(this).before($(this).data('fields').replace(regexp,time));
+		$(this).before($(this).data('fields').replace(regexp, time));
 		event.preventDefault();
-		
-		$( ".committee_member_names" ).autocomplete({
-		 source: committee_member_names_list
-		});	
+
+		$(".committee_member_names").autocomplete({
+			source: committee_member_names_list
+		});
 	});
-	
-	
 });
