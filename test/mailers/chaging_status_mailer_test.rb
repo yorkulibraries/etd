ad732def30f8cd39a992754c5fe7a874fe6114ec
Luis Zambrano <lzambra2@yorku.ca>
2024-02-20 16:22:36 -0500

    Notify student when thesis is published. #31

--- test/mailers/chaging_status_mailer_test.rb
+++ test/mailers/chaging_status_mailer_test.rb
@@ -7,6 +7,6 @@ class ChagingStatusMailerTest < ActionMailer::TestCase
     assert_equal 'The status of your thesis has been changed.', mail.subject
     assert_equal [EMAIL], mail.to
     assert_equal ['noreply@yorku.ca'], mail.from
-    assert_match 'Status of thesis have changed to "Published"', mail.body.encoded
+    assert_match 'The status of the thesis has changed to "Published".', mail.body.encoded
   end
 end
