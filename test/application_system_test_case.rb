f6265c88f385115406705a646f6194690dd17090
Tuan Nguyen <amtuannguyen@gmail.com>
2024-07-17 21:38:57 -0400

    increase Capybara max wait time and use headless chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: {
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: {
     browser: :remote,
     url: "http://#{ENV.fetch('SELENIUM_SERVER')}:4444"
   }
