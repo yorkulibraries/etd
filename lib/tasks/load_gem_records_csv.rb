76d15bfcf17e84b39c1e96d741d3a5dd521f3ab8
Tuan Nguyen <amtuannguyen@gmail.com>
2024-07-23 11:08:07 -0400

    resolves #58

--- lib/tasks/load_gem_records_csv.rb
+++ lib/tasks/load_gem_records_csv.rb
@@ -14,26 +14,26 @@ class LoadGemRecordsCSV
     converter = lambda { |header| header.downcase }
     CSV.foreach(filename, headers: true, header_converters: converter) do |row|
       seqgradevent = row['seqgradevent'].strip
-      unless GemRecord.find_by_seqgradevent(seqgradevent)
+      unless gr = GemRecord.find_by_seqgradevent(seqgradevent)
         gr = GemRecord.new
-        gr.seqgradevent = seqgradevent
-        gr.studentname = row['studentname'].strip
-        gr.sisid = row['sisid'].strip
-        gr.emailaddress = row['emailaddress'].strip
-        gr.eventtype = row['eventtype'].strip
-        gr.eventdate = row['eventdate'].strip
-        gr.examresult = row['examresult'].strip
-        gr.title = row['title'].strip
-        gr.program = row['program'].strip
-        gr.superv = row['superv'].strip
-        gr.examdate = row['examdate'].strip
+      end
+      gr.seqgradevent = seqgradevent
+      gr.studentname = row['studentname'].strip
+      gr.sisid = row['sisid'].strip
+      gr.emailaddress = row['emailaddress'].strip
+      gr.eventtype = row['eventtype'].strip
+      gr.eventdate = row['eventdate'].strip
+      gr.examresult = row['examresult'].strip
+      gr.title = row['title'].strip
+      gr.program = row['program'].strip
+      gr.superv = row['superv'].strip
+      gr.examdate = row['examdate'].strip
 
-        if gr.save!(validate: false)
-          count += 1
-        else
-          warn('Error: Load Gem Records Save Failed!')
-          warn("Error: #{gr.errors.inspect}")
-        end
+      if gr.save!(validate: false)
+        count += 1
+      else
+        warn('Error: Load Gem Records Save Failed!')
+        warn("Error: #{gr.errors.inspect}")
       end
     rescue StandardError => e
       warn("ERROR: #{e}")
