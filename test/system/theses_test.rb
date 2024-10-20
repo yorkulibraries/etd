428eef3b6cbc09600f614338cba54d13557eb158
Tuan Nguyen <amtuannguyen@gmail.com>
2024-10-19 21:15:42 -0400

    add link text for the remove committee member button that is only seen by screen readers (sr-only)

--- test/system/theses_test.rb
+++ test/system/theses_test.rb
@@ -80,7 +80,7 @@ class ThesesTest < ApplicationSystemTestCase
 
     assert_selector 'a[data-delete="test2, test1 (Committee Member)"]', visible: true
     
-    find('a[data-delete="test2, test1 (Committee Member)"]').click(x: 12, y: 12)
+    click_link('Remove test2, test1 (Committee Member)')
 
     assert_no_selector 'span', text: 'test2, test1'
   end
