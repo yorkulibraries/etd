7566c99f1799464cc49053b4a9df418add73f9a7
Tuan Nguyen <amtuannguyen@gmail.com>
2025-02-23 10:45:22 -0500

    use app config for the default email from address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,5 +2,8 @@
 
 class ApplicationMailer < ActionMailer::Base
   default from: 'noreply@yorku.ca'
+  if AppSettings.email_from
+    default from: AppSettings.email_from
+  end
   layout 'mailer'
 end
